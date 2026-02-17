34e09b11c90f8e1c647b86b6d373267c4208aa70
karan89200 <bunk4code@gmail.com>
2026-02-17 22:06:16 +0530

    first time commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category,Blog
+# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('title','category','status','is_featured')
+    search_fields = ('id','title','category__category_name','status')
+
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
