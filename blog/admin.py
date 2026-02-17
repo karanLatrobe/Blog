479a31d5910fa142e59ad73aa04e867f36502bb0
karan89200 <bunk4code@gmail.com>
2026-02-17 23:53:55 +0530

    2 :20 minutes completed

--- blog/admin.py
+++ blog/admin.py
@@ -6,6 +6,7 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
     list_display = ('title','category','status','is_featured')
     search_fields = ('id','title','category__category_name','status')
+    list_editable = ('is_featured',)
 
 admin.site.register(Category)
 admin.site.register(Blog,BlogAdmin)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,13 @@
 
 from django.shortcuts import render
+from blog.models import Category , Blog
+
 
 def home(request):
-    return render(request,'home.html')
+    category_list = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True).order_by('-updated_at')
+    posts = Blog.objects.filter(is_featured = False).order_by('-updated_at')
+    context = {
+        'categories':category_list, 'featured_posts':featured_posts, 'posts':posts
+    }
+    return render(request,'home.html',context)
